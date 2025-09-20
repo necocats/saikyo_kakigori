c8f6f4567f91abae032dd9bd9c5f42bab375f8b2
39rep <okome3317@gmail.com>
2025-09-20 16:34:50 +0900

    広告バナー作成

---
+++ app/components/AdBanner.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./AdBanner.css";
+
+const AdBanner: React.FC = () => {
+  const [visible, setVisible] = useState(true);
+
+  const handleClick = () => {
+    setVisible(false);
+  };
+
+  if (!visible) return null;
+
+  return (
+    <a
+      className="ad-banner"
+      href="https://nlab.itmedia.co.jp/cont/articles/3271793/"
+      target="_blank"
+    >
+      <div
+        className="ad-banner__close-button absolute top-0 right-0"
+        onClick={handleClick}
+      >
+        ×
+      </div>
+      <span className="ad-banner__text">
+        🙍‍♂️ 人間 vs コンピュータオセロ 勝負の行方は 🤖
+      </span>
+      <div className="ad-banner__link">詳しくはこちら</div>
+    </a>
+  );
+};
+
+export default AdBanner;
--- app/routes/order.$storeId._index.tsx
+++ app/routes/order.$storeId._index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Agreement } from "../components/Agreement";
 import { Questionnaire } from "../components/Questionnaire";
 import { OrderForm } from "../components/OrderForm";
+import AdBanner from "../components/AdBanner";
 
 export default function OrderPage() {
   const [agreedToQuestionnaire, setAgreedToQuestionnaire] = useState(false);
@@ -13,18 +14,23 @@ export default function OrderPage() {
   };
 
   return (
-    <main className="mx-auto max-w-sm p-4 min-h-[100dvh] flex flex-col justify-start">
-      <h1 className="text-2xl font-bold text-center">かき氷注文システム</h1>
-      {!agreedToQuestionnaire ? (
-        <Agreement onAgree={() => setAgreedToQuestionnaire(true)} />
-      ) : !questionnaireCompleted ? (
-        <Questionnaire
-          onComplete={() => setQuestionnaireCompleted(true)}
-          onRetry={handleRetry}
-        />
-      ) : (
-        <OrderForm />
-      )}
-    </main>
+    <>
+      <main className="mx-auto max-w-sm p-4 min-h-[100dvh] flex flex-col justify-start">
+        <h1 className="text-2xl font-bold text-center">かき氷注文システム</h1>
+        {!agreedToQuestionnaire ? (
+          <Agreement onAgree={() => setAgreedToQuestionnaire(true)} />
+        ) : !questionnaireCompleted ? (
+          <Questionnaire
+            onComplete={() => setQuestionnaireCompleted(true)}
+            onRetry={handleRetry}
+          />
+        ) : (
+          <>
+            <OrderForm />
+            <AdBanner />
+          </>
+        )}
+      </main>
+    </>
   );
 }
